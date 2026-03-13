bbf3da3a2598a0d7ba6cb0486286f316347e7b1f
Tymon Wozniak <tymon.student@gmail.com>
2026-03-13 17:40:20 +0100

    refactor(scene_changer): changed from AnimationPlayer to Tween
    - removed AnimationPlayer from scene
    
    - implemented fade in/out using Tween
    
    - added mouse blocking system

--- autoloads/scene_changer.gd
+++ autoloads/scene_changer.gd
@@ -1,16 +1,33 @@
 extends CanvasLayer
 
-@onready var animation_player = $AnimationPlayer
 @onready var color_rect = $Background
+
+const FADE_DURATION: float = 0.3
 
 func _ready():
 	color_rect.color = Color(0.0, 0.0, 0.0, 0.0)
+	color_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
 
 func change_scene_smooth(path: String):
-	animation_player.play("fade_to_black")
-	await animation_player.animation_finished
+	# Block interface
+	color_rect.mouse_filter = Control.MOUSE_FILTER_STOP
+
+	# Fade to black
+	var fade_in = create_tween()
+	fade_in.tween_property(color_rect, "color", Color(0.0, 0.0, 0.0, 1.0), FADE_DURATION)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	
+	await fade_in.finished
+
+	# Change scene
 	get_tree().change_scene_to_file(path)
-	
-	animation_player.play("fade_from_black")
-	await animation_player.animation_finished
+
+	# Fade from black
+	var fade_out = create_tween()
+	fade_out.tween_property(color_rect, "color", Color(0.0, 0.0, 0.0, 0.0), FADE_DURATION)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+	await fade_out.finished
+
+	# Unblock interface
+	color_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
