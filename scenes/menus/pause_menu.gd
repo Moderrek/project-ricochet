bafab905f9ea4a4dac1d0c9d90d9ec9cd94d8e38
Tymon Wozniak <tymon.student@gmail.com>
2026-03-14 09:01:45 +0100

    feat(pause_menu): added singleton pause menu

---
+++ scenes/menus/pause_menu.gd
@@ -0,0 +1,48 @@
+extends CanvasLayer
+
+@onready var resume_button = $VBoxContainer/ResumeButton
+@onready var main_menu_button = $VBoxContainer/MainMenuButton
+@onready var quit_button = $VBoxContainer/QuitButton
+
+func _ready() -> void:
+	if resume_button:
+		resume_button.pressed.connect(_on_resume_pressed)
+	if main_menu_button:
+		main_menu_button.pressed.connect(_on_main_menu_pressed)
+	if quit_button:
+		quit_button.pressed.connect(_on_quit_pressed)
+
+	hide()
+
+	if OS.has_feature("web") and quit_button:
+		quit_button.hide()
+	
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if GameManager.is_game_running:
+			toggle_pause()
+
+func toggle_pause() -> void:
+	var is_paused = not get_tree().paused
+	get_tree().paused = is_paused
+	visible = is_paused
+
+	if is_paused:
+		if resume_button:
+			resume_button.grab_focus()
+
+func _on_resume_pressed() -> void:
+	toggle_pause()
+
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	hide()
+
+	GameManager.is_game_running = false
+	GameManager.is_timer_active = false
+
+	SceneChanger.change_scene_smooth("res://scenes/menus/main_menu.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
